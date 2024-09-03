afcfc340a79b37ef18b5517b84a3c3987f6a9222
Wei-Ju Huang <wei-ju.huang@stickee.co.uk>
2024-09-03 15:09:43 +0100

    Basic frontend

---
+++ resources/views/components/podcast-card-wide.blade.php
@@ -0,0 +1,20 @@
+<x-panel class="flex gap-x-6">
+    <div>
+        <x-creator-logo></x-creator-logo>
+    </div>
+    
+    <div class="flex-1 flex flex-col">
+        <a class="self-start text-sm text-gray-400">Laracasts</a>
+        <h3 class="font-bold text-xl mt-3 group-hover:text-blue-600 transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm text-gray-400 mt-auto">Full time - From $60,000</p>
+    </div>
+
+    <div>
+        <div>
+            <x-tag size="small">Tech</x-tag>
+            <x-tag size="small">Art</x-tag>
+            <x-tag size="small">News</x-tag>
+        </div>
+    </div>
+    
+</x-panel>
---
+++ resources/views/components/podcast-card.blade.php
@@ -0,0 +1,17 @@
+<x-panel class="flex flex-col text-center">
+    <div class="self-start text-sm">Laracasts</div>
+    <div class="py-8">
+        <h3 class="font-bold group-hover:text-blue-600 text-xl transition-colors duration-300">Video Producer</h3>
+        <p class="text-sm mt-4">Full time - From $60,000</p>
+    </div>
+    <div class="flex justify-between items-center mt-auto">
+        <div>
+            <x-tag size="small">Tech</x-tag>
+            <x-tag size="small">Art</x-tag>
+            <x-tag size="small">News</x-tag>
+        </div>
+
+        <x-creator-logo :width="42"></x-creator-logo>
+    </div>
+    
+</x-panel>
