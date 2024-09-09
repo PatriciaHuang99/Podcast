644db06dceb6bbd8254a5ad771bbc4722f286dbd
Wei-Ju Huang <wei-ju.huang@stickee.co.uk>
2024-09-09 15:31:48 +0100

    Create Register and Login and Sign up

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -9,7 +9,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="bg-black text-white font-hanken-grotesk">
+<body class="bg-black text-white font-hanken-grotesk pb-20">
     <div class="px-10">
         <nav class="flex justify-between items-center py-4 border-b border-white/10">
             <div>
@@ -23,9 +23,19 @@
                 <a href="">Salaries</a>
                 <a href="">Companies</a>
             </div>
-            <div>
-                <a href="">post a podcast</a>
-            </div>
+
+            @auth
+                <div class="space-x-6 font-bold">
+                    <a href="/podcasts/create">post a podcast</a>
+                </div>
+            @endauth
+
+            @guest
+                <div class="space-x-6 font-bold">
+                    <a href="/register">Sign Up</a>
+                    <a href="/login">Log In</a>
+                </div>
+            @endguest
         </nav>
         <main class="mt-10 max-w-[999px] mx-auto">
             {{ $slot }}
